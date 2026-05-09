f546dfd43c1c403f59a7baba409c5ea45b734044
Ayeshataj-2002 <113886411+Ayeshataj-2002@users.noreply.github.com>
2026-05-09 16:15:15 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cs
@@ -0,0 +1,35 @@
+public class Solution {
+
+    public string Encode(IList<string> strs) {
+        StringBuilder encoded = new StringBuilder();
+
+        foreach(string str in strs)
+        {
+            encoded.Append(str.Length);
+            encoded.Append('#');
+            encoded.Append(str);
+        }
+        return encoded.ToString();
+    }
+
+    public List<string> Decode(string s) {
+        List<string> result = new List<string>();
+        int i = 0;
+        while(i < s.Length)
+        {
+            int j = i;
+
+            while(s[j] != '#')
+            {
+                j++;
+            }
+
+            int length = int.Parse(s.Substring(i, j - i));
+            j++;
+            string word = s.Substring(j, length);
+            result.Add(word);
+            i = j + length;
+        }
+        return result;
+   }
+}
